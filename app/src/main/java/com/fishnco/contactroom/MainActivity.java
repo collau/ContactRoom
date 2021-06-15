1cd8e169944880cc63263a8c286e38eabe226406
junyi <collau@hotmail.com>
2021-06-15 23:30:52 +0800

    8. Adding Custom Contacts Via the Contact Activity - Revert Main Activity as Launcher Activity - Floating Button to bring us to NewContact Activity - Upon Save, return to Main Activity to show that result has been saved and displayed

--- app/src/main/java/com/fishnco/contactroom/MainActivity.java
+++ app/src/main/java/com/fishnco/contactroom/MainActivity.java
@@ -1,20 +1,25 @@
 package com.fishnco.contactroom;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.fishnco.contactroom.model.Contact;
 import com.fishnco.contactroom.model.ContactViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int NEW_CONTACT_ACTIVITY_REQUEST_CODE = 1;
     private ContactViewModel contactViewModel;
     private TextView textView;
 
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         contactViewModel = new ViewModelProvider.AndroidViewModelFactory(MainActivity.this.getApplication())
                 .create(ContactViewModel.class);
 
+        //Listens for all the changes
         contactViewModel.getAllContacts().observe(this, new Observer<List<Contact>>() {
             @Override
             public void onChanged(List<Contact> contacts) {
@@ -43,5 +49,27 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        FloatingActionButton buttonAdd = findViewById(R.id.button_addContact);
+        buttonAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, NewContact.class);
+                startActivityForResult(intent, NEW_CONTACT_ACTIVITY_REQUEST_CODE);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == NEW_CONTACT_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+            assert data != null;
+            Contact contact = new Contact(data.getStringExtra(NewContact.NAME_REPLY), data.getStringExtra(NewContact.OCCUPATION_REPLY));
+            ContactViewModel.insert(contact);
+
+            Log.d("MainActivity", "onActivityResult: " + data.getStringExtra(NewContact.NAME_REPLY));
+            Log.d("MainActivity", "onActivityResult: " + data.getStringExtra(NewContact.OCCUPATION_REPLY));
+        }
     }
 }
--- app/src/main/java/com/fishnco/contactroom/NewContact.java
+++ app/src/main/java/com/fishnco/contactroom/NewContact.java
@@ -1,8 +1,6 @@
 package com.fishnco.contactroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -10,10 +8,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
 import com.fishnco.contactroom.model.Contact;
 import com.fishnco.contactroom.model.ContactViewModel;
 
 public class NewContact extends AppCompatActivity {
+    public static final String NAME_REPLY = "name_reply";
+    public static final String OCCUPATION_REPLY = "occupation_reply";
     private EditText editText_name;
     private EditText editText_occupation;
     private Button buttonSave;
@@ -35,15 +38,26 @@ public class NewContact extends AppCompatActivity {
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent replyIntent = new Intent();
 
                 if (!TextUtils.isEmpty(editText_name.getText()) && !TextUtils.isEmpty(editText_occupation.getText())) {
-                    Contact contact = new Contact(editText_name.getText().toString(), editText_occupation.getText().toString());
+                    String name = editText_name.getText().toString();
+                    String occupation = editText_occupation.getText().toString();
+
+                    replyIntent.putExtra(NAME_REPLY, name);
+                    replyIntent.putExtra(OCCUPATION_REPLY, occupation);
+                    setResult(RESULT_OK, replyIntent);
+
+//                    Contact contact = new Contact(editText_name.getText().toString(), editText_occupation.getText().toString());
+
 
                     //insert method is a static method, just use the actual class
-                    ContactViewModel.insert(contact);
+//                    ContactViewModel.insert(contact);
                 } else {
-                    Toast.makeText(NewContact.this, R.string.empty, Toast.LENGTH_SHORT).show();
+                    setResult(RESULT_CANCELED, replyIntent);
+//                    Toast.makeText(NewContact.this, R.string.empty, Toast.LENGTH_SHORT).show();
                 }
+                finish();
 
             }
         });
